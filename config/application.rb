5d2916763d1e4000d73ff57a0334a1db51bc069d
Kenneth Ashley <Kashley@Kenneths-MacBook-Air.local>
2017-05-01 00:25:05 -0500

    More Push

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,7 @@ module KwashleyProjectCis4339
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.assets.initialize_on_precompile = false
-    config.serve_static_assets = true
+ 
   end
 end
 
